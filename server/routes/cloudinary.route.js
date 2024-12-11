fb3a2b0b437eda3b516247795f0bb8960a63986a
Surf-Punk <122007206+Surf-Punk@users.noreply.github.com>
2024-12-12 01:01:04 +0500

    Delete server directory

--- server/routes/cloudinary.route.js
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-import { addImage } from '../controllers/cloudinary.controller.js';
-import { verifyToken } from '../lib/middleware.js';
-const router = express.Router();
-router.post('/upload', verifyToken, addImage);
-export default router;
